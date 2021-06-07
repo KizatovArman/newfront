226321c9ecb1e78b2871b83e10efa9250581747a
Kizatov_Arman <kizatov.armashka.05@gmail.com>
2021-06-07 06:31:56 +0600

    pls pls pls

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static('./dist/plagiarism-checker-front'));
+app.use(express.static(__dirname + '/angularapp'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: './dist/plagiarism-checker-front'}),
+    res.sendFile(path.join(__dirname)+'/angularapp/index.html'),
 );
 
 app.listen(process.env.PORT || 8080);
